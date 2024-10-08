6da9d1991d88e5e9e93ac4d3079200add95f6efd
elizaOKa <elizkooo@yahoo.com.br>
2024-10-08 20:35:51 -0300

    inserção de dados

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
+/* REenderização da lista de dados*/
+const listBook = document.querySelector('#book-list');
 
+function renderList(doc){
+    let li= document.createElement('li');
+    let autor= document.createElement('span');
+    let titulo= document.createElement('span');
 
+    autor.textContent =doc.autor;
+    titulo.textContent =doc.titulo;
+
+    li.appendChild(titulo);
+    li.appendChild(autor);
+
+    listBook.appendChild(li);
+
+}
+
+/* lista da coleção firestore*/
+
+db.collection('libri-collection')
+    .get()
+    .then((snapshot)=>{
+        // console.log(snapshot.docs)
+        snapshot.docs.forEach(
+            doc =>{
+                console.log(doc.data())
+                renderList(doc.data())
+            }
+
+        )
+    });
+    /*inserção de dados*/
+    const form = document.querySelector('#add-book-form');
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        alert('Formulario funcionando')
+    });
